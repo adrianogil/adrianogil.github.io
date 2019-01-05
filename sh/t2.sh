837f3ea118570d6a8ead8db94958f5d321fe884e
Adriano Gil <adrianogil.san@gmail.com>
2019-01-05 06:45:05 -0400

    Update termux installer script 2

--- sh/t2.sh
+++ sh/t2.sh
@@ -1,5 +1,5 @@
 # DOT FILES SETUP (1/2)
-DOTFILES_DIR=$HOME/.dotfiles
+export DOTFILES_DIR=$HOME/.dotfiles
 git clone git@github.com:adrianogil/dotfiles.git $DOTFILES_DIR
 
 cd $DOTFILES_DIR
@@ -12,6 +12,7 @@ git clone git@github.com:adrianogil/gil-tools.git $GILTOOLS_DIR
 cd $GILTOOLS_DIR
 
 pkg install python2 -y
+pkg install tmux -y
 
 python2 src/python/gil_install.py -i
 
