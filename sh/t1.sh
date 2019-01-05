4f5fcc66bcb29796614af4aeeaf80b0a70fdabed
Adriano Gil <adrianogil.san@gmail.com>
2019-01-04 23:10:48 -0400

    Update termux installer script 1

--- sh/t1.sh
+++ sh/t1.sh
@@ -8,5 +8,6 @@ cd $installer_folder
 
 ssh-keygen -t rsa -N "" -f $HOME/.ssh/my.key
 
-mkdir /sdcard/sshkeys/
-cp $HOME/.ssh/*.pub /sdcard/sshkeys/
+termux-setup-storage
+
+cp $HOME/.ssh/*.pub /sdcard/
